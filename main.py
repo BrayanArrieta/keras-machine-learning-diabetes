d741a016213ebcd704d788b5d1581dfc086fc5d2
Brayan Arrieta <55404285+brayanarrieta@users.noreply.github.com>
2018-04-01 21:08:59 -0600

    Fix bug in load csv

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy
+
+
 # fix random seed for reproducibility
 numpy.random.seed(7)
 
 
-# Load data
 # load pima indians dataset
-dataset = numpy.loadtxt("diabetes.csv", delimiter=",")
+dataset = numpy.genfromtxt("diabetes.csv", delimiter=",")
 # split into input (X) and output (Y) variables
 X = dataset[:,0:8]
 Y = dataset[:,8]
+
+# Define model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+# Compile the model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+model.fit(X, Y, epochs=150, batch_size=10)
